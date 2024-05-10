0243d3458f83684453130d9b9d359407a85eefb2
Ali Sharif <alisharif29@gmail.com>
2024-05-09 19:39:44 -0700

    Added react query dependency

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import './App.css'
 import {RouterProvider} from "react-router-dom";
 import {router} from "./routes.tsx";
+import ReactQuery from "./components/TanstackProvider/TanstackProvider.tsx";
 
 function App() {
 
     return (
+        <ReactQuery>
         <RouterProvider router={router}/>
+        </ReactQuery>
     )
 }
 
