1735f0ab7e6a8eb0725caab4ec2b59b6a89007f9
Ali Sharif <alisharif29@gmail.com>
2024-05-16 22:26:03 -0700

    Updated routes.tsx

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,16 +1,30 @@
 import {createBrowserRouter} from "react-router-dom";
 import Home from "./pages/home/Home.tsx";
 import Login from "./pages/login/Login.tsx";
+import Signup from "./pages/signup/Signup.tsx";
+import Layout from "./layout/Layout.tsx";
 
 
 export const router = createBrowserRouter([
     {
         path: "/",
-        element: <Login/>,
+        element: <Layout/>,
+        children: [
+            {
+                path: "/home",
+                element: <Home/>,
+            }
+
+        ]
     },
 
     {
-        path: "/home",
-        element: <Home/>,
+        path: "/login",
+        element: <Login/>
+    },
+
+    {
+        path: "/sign-up",
+        element: <Signup/>
     }
 ])
