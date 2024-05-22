3106bfc7dd1bae5c4e36c572acda4c09dc175e10
Ali Sharif <alisharif29@gmail.com>
2024-05-21 17:09:26 -0700

    Created useUserContext hook to utilize UserContext

---
+++ src/hooks/useUserContext.tsx
@@ -0,0 +1,12 @@
+import {useContext} from "react";
+import {UserContext} from "../contexts/UserContext.tsx";
+
+export const useUserContext = () => {
+    const context = useContext(UserContext);
+    if(!context) {
+        throw new Error(
+            'useUserContext must be used with a UserContextProvider'
+        )
+    }
+    return context;
+}
