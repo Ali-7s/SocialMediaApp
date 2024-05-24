63c60df3a4dc6c992d76a75abbd5eae1f0fd3f0d
Ali Sharif <alisharif29@gmail.com>
2024-05-23 20:27:14 -0700

    Added functions for create, delete, update post functionality

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,5 +1,5 @@
-import axios, {AxiosRequestConfig} from "axios";
-import {Post, PostRequest, User} from "../types.tsx";
+import axios, { AxiosRequestConfig } from "axios";
+import { Post, PostRequest, UpdatePostRequest, User} from "../types.tsx";
 
 export const API_URL = 'http://localhost:8080/api';
 
@@ -18,9 +18,25 @@ export async function getPosts(): Promise<Post[]> {
     return response.data.data.posts;
 }
 
-export async function createNewPost(  post  : PostRequest) {
-    return await axios.post(`${API_URL}/post/add`, post, getAxiosConfig());
+export async function deletePost(id: number)  {
+    return await axios.delete(`${API_URL}/post/delete/${id}`, getAxiosConfig());
 }
+
+export async function createNewPost(  post  : PostRequest) {
+    if(post.content === "") {
+        throw new Error("Post content cannot be empty");
+    }
+    return await axios.post(`${API_URL}/post/add`, post, getAxiosConfig())
+}
+
+export async function updatePost( post : UpdatePostRequest) {
+    if(post.content === "") {
+        throw new Error("Post content cannot be empty");
+    }
+
+    return await axios.patch(`${API_URL}/post/update`, post, getAxiosConfig())
+}
+
 
 export async function getAuthedUser(): Promise<User | undefined> {
     try {
