3cb797e87f44834eb8b347bb87b3ca2859a58ac4
Ali Sharif <alisharif29@gmail.com>
2024-05-30 19:07:43 -0700

    Update handleSignup function

--- src/pages/signup/Signup.tsx
+++ src/pages/signup/Signup.tsx
@@ -1,6 +1,6 @@
 import {Box, Button, TextField} from "@mui/material";
 import React, {useState} from "react";
-import axios from "axios";
+import axios, {AxiosError} from "axios";
 import {API_URL} from "../../api/api.ts";
 import {useNavigate} from "react-router-dom";
 import {UserRegistration} from "../../types.tsx";
